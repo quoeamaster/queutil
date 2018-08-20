6c9fee0aa658733e7c6d3de4c5d1c0e18c348874
quoeamaster <quoeamaster@gmail.com>
2018-08-20 19:01:55 +0800

    2018-08-20a

--- StructsAndInterfaces.go
+++ StructsAndInterfaces.go
@@ -1,5 +1,7 @@
 package queutil
 
+import "encoding/json"
+
 // acts as a common interface for "releasable" object(s)
 type IReleasable interface {
     // operations to be done before closing the associated object.
@@ -26,3 +28,36 @@ type ILogger interface {
 type IJsonStringAble interface {
     JsonString() string
 }
+
+// ***********************************
+// **   Value Object definition(s)  **
+// **   sharable containers for     **
+// **   common data sharing between **
+// **   api calls or processes      **
+// ***********************************
+
+
+// BrokerSeed related information
+type BrokerSeedVO struct {
+    CanJoin bool                        `json:"CanJoin"`
+    IsMasterReady bool                  `json:"IsMasterReady"`
+    IsDataReady bool                    `json:"IsDataReady"`
+
+    // is the broker an active master already (no election needed)
+    IsActiveMaster bool                 `json:"IsActiveMaster"`
+
+    BrokerName string                   `json:"BrokerName"`
+    BrokerCommunicationAddr string      `json:"BrokerCommunicationAddr"`
+    BrokerId string                     `json:"BrokerId"`
+
+    DiscoveryBrokerSeeds []string       `json:"DiscoveryBrokerSeeds"`
+}
+// method to provide a json-fy string based on the contents of the current instance
+func (b *BrokerSeedVO) JsonString() string {
+    buf, err := json.Marshal(b)
+    if err != nil {
+        panic(err)
+    }
+    return string(buf)
+}
+
